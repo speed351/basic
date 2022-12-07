1f5c88e3d2afa5aed9926c024fce921609bf6c76
park <zpzp351@naver.com>
2022-12-07 11:22:08 +0900

    component format change(function->class)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  let myName ="park";
 
-  return (
-    <div>
-      <h1>index page</h1>
-      {/* myName이라는 변수 안에 있는 내용 사용하기 */}
-      <p>my name is <strong>{myName}</strong></p>
-      <button className='btn btn-primary'>click</button>
-    </div>
+class App extends Component{
+  render(){
+    let myName="Park";
+    return(
+      <div>
+        <h1>index page</h1>
+        {/* myName이라는 변수 안에 있는 내용 사용하기 */}
+        <p>my name is <strong>{myName}</strong></p>
+        <button className="btn btn-primary" onClick={()=>{
+          alert("버튼을 눌렀넹");
 
-  );
+        }}>Click!!</button>
+      </div>
+
+    );
+  }
+
+
 }
 
 export default App;
