ecc1f652e3f37594e6fce6c3752bde741d7b3f00
park <zpzp351@naver.com>
2022-12-07 11:08:42 +0900

    css added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let myName ="park";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>index page</h1>
+      {/* myName이라는 변수 안에 있는 내용 사용하기 */}
+      <p>my name is <strong>{myName}</strong></p>
+      <button className='btn btn-primary'>click</button>
     </div>
+
   );
 }
 
